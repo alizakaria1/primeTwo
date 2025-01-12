2d59c5e1a74c89fbb91ecc3a27461f20bd613869
ali zakaria <ali-zakaria1@outlook.com>
2025-01-12 11:44:43 +0200

    fix docker file to include the Images folder

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef5da8a9c1f320214100a01af1b31725c798974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1248d4c2e89fffb7fbfff374e77545e29ba3e7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
